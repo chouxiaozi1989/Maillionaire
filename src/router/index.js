06ca54585af6ec80c1dcb562039993fcc971c6ee
github <caiyi1989@qq.com>
2025-11-08 21:45:33 +0800

    fix: ensure router works under file protocol

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAccountStore } from '@/stores/account'
 
 /**
@@ -81,7 +81,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
